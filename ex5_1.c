6eb52b48bb78ba0101a547974f79da1e12ab512b
sua789 <145026082+sua789@users.noreply.github.com>
2024-04-30 11:24:21 +0900

    Add files via upload

---
+++ ex5_1.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "stackS.h"
+
+int main(void) {
+	element item;
+	printf("\n** 순차 스택 연산 **\n");
+	printStack();
+	push(1); printStack();
+	push(2); printStack();
+	push(3); printStack();
+
+	item = peek(); printStack();
+	printf(" peek => %d", item);
+
+	item = pop(); printStack();
+	printf("\t pop => %d", item);
+
+	item = pop(); printStack();
+	printf("\t pop => %d", item);
+
+	item = pop(); printStack();
+	printf("\t pop => %d", item);
+
+	getchar(); return 0;
+}
